45fe72a6caf2b90d9ccb77b6834e8ca433702838
Weston Hanners <weston.hanners@cookmedical.com>
2013-05-02 14:46:09 -0400

    Code cleanup and commenting. Made usage clear.

--- Plopper/HSViewController.m
+++ Plopper/HSViewController.m
@@ -28,23 +28,21 @@
     if (p.x < 0){
         NSLog(@"out");
     }
-    HSPlopperViewController *plopper = [[HSPlopperViewController alloc] initWithSize:CGSizeMake(150, 100)];
+    HSPlopperViewController *plopper = [[HSPlopperViewController alloc] initWithSize:CGSizeMake(150, 100)];  // Init our Plopper Instance
     plopper.superView = self;                                                                                // Setup SuperView (this is to prevent the plopper from rendering outside the view).
     plopper.displayTitle = [NSString stringWithFormat:@"Plop %d", self.ploppers.count];
-    plopper.view.center = p;
-    plopper.delegate = self;
-    NSLog(@"Plopper %@ at (%f,%f)", plopper, plopper.frame.origin.x, plopper.frame.origin.y);
-
-    [self.ploppers addObject:plopper];
-    [self.view addSubview:plopper.view];
+    plopper.view.center = p;                                                                                 // Position the Plopper.
+    plopper.delegate = self;                                                                                 // Register to recieve callbacks using the <HSPlopperDelegate> protocol.
+    [self.ploppers addObject:plopper];                                                                       // Add Plopper to an array to keep track.
+    [self.view addSubview:plopper.view];                                                                     // Add Plopper to our view, animation is automatic.
 }
 
 - (void)wasDismissed:(HSPlopperViewController *)plopper {
-    [self.ploppers removeObject:plopper];
+    [self.ploppers removeObject:plopper];                                                                    // Remove Plopper when it is hidden
 }
 
 - (void)wasShown:(HSPlopperViewController *)plopper {
-    NSLog(@"Plopper %@ at (%f,%f)", plopper, plopper.frame.origin.x, plopper.frame.origin.y);
+    NSLog(@"Plopper %@ at (%f,%f)", plopper, plopper.view.frame.origin.x, plopper.view.frame.origin.y);
 }
 
 @end
